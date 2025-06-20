2e79b109fd359da358f9c311a1a5f58d19376ac2
abhaycoderarmy <2320403202@stu.manit.ac.in>
2025-06-20 17:50:18 +0530

    fixed naming

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -104,7 +104,7 @@ export default function Signup() {
               {/* Email Input */}
               <div className="relative">
                 <label className="block text-sm font-medium text-gray-700 mb-2">
-                  Email Address
+                  Email Addresses
                 </label>
                 <div className="relative">
                   <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
